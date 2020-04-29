c8f3c24d20a4e1230c4ffc9433725ed43a734f45
thedannysoto <87dsoto@gmail.com>
2020-04-29 12:30:43 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin (record) {
+  const winCheck = record.find(function(year) {
+    if (year.result === "W") {
+      return year;
+    } 
+  });
+  if (winCheck) {
+    return winCheck.year;
+  } else {
+    let x;
+    return x;
+  }
+}
